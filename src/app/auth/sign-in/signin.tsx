27456bc96c423de90fc50c3060ebc4ac3d5bdd02
Jackson Zheng <60581068+JacksonZ03@users.noreply.github.com>
2024-05-13 12:29:06 +0100

    Step 3: - Create the basic google sign in form. - Create a pages directory in sass folder and add the signin.scss file to that directory. Add the basic styles needed for only the form and the Sign in with Google button. - Create the googleSignInServerAction.ts file with handleGoogleSignIn() function with a dummy print statement.

--- src/app/auth/sign-in/signin.tsx
+++ src/app/auth/sign-in/signin.tsx
@@ -1,9 +1,22 @@
 "use client";
 
+import { FcGoogle } from "react-icons/fc";
+import { handleGoogleSignIn } from "@/src/lib/auth/googleSignInServerAction";
+
 export const SignInPage: React.FC = () => {
   return (
-    <div>
-      <h1>Sign in</h1>
+    <div className="signin-page">
+      <div className="signin-card">
+        <h2>Sign In</h2>
+        <div className="form-container">
+          <div className="social-logins">
+            <button className="google" onClick={() => handleGoogleSignIn()}>
+              <FcGoogle className="google-icon" />
+              Sign in with Google
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/lib/auth/googleSignInServerAction.ts
@@ -0,0 +1,10 @@
+"use server";
+
+export const handleGoogleSignIn = async () => {
+  try {
+    // TODO: Handle Google SignIn
+    console.log("Google SignIn");
+  } catch (error) {
+    throw error;
+  }
+};
